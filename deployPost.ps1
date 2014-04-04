951c1f9ecad67b00b714f5cc8167224fd378dff2
erichgess <erich.g.ess@gmail.com>
2014-04-03 22:14:39 -0700

    updating deploy script

--- deployPost.ps1
+++ deployPost.ps1
@@ -3,4 +3,7 @@
 rm -r c:\temp\*
 xcopy .\public\* c:\temp /E
 git checkout master
-xcopy c:\temp\* .\public /E /Y
+xcopy c:\temp\* .\ /E /Y
+git add .
+git commit -m "deploying posts"
+git push origin
